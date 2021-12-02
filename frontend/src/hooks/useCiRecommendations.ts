194ee41c7f8ac0bfffcab26b3a4ae748d2168035
Guillaume Chérel <guillaume.cherel@cosimus.com>
2021-12-02 18:24:17 +0100

    Use useCallback hook.

--- frontend/src/hooks/useCiRecommendations.ts
+++ frontend/src/hooks/useCiRecommendations.ts
@@ -1,28 +1,31 @@
 import {Ci} from "utils/helpers/Ci";
 import {CiReco, ciReco} from "utils/helpers/CiReco";
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import {fetchCiRandom, fetchCiReco} from 'services/backend';
 
 function useCiRecommendations(selectedCis: Ci[], nReco: number,
     dataVersion: string | undefined) {
   const [ciRecoState, setCiRecoState] = useState<CiReco | undefined>(undefined);
 
-  const drawRandomReco = (): void => {
-    if (dataVersion !== undefined) {
-      fetchCiRandom(dataVersion, nReco * 3, selectedCis)
-        .then(cis => {
-          if (cis.length === 0) {
-            console.error("Received 0 random CI from backend.");
-          } else {
-              const cir = ciReco(
-                cis.slice(0,nReco), 
-                cis.slice(nReco,nReco * 2), 
-                cis.slice(nReco * 2, nReco * 3));
-            setCiRecoState(cir);
-          }
-        });
-    }
-  };
+  const drawRandomReco = useCallback(
+    () => {
+      if (dataVersion !== undefined) {
+        fetchCiRandom(dataVersion, nReco * 3, selectedCis)
+          .then(cis => {
+            if (cis.length === 0) {
+              console.error("Received 0 random CI from backend.");
+            } else {
+                const cir = ciReco(
+                  cis.slice(0,nReco), 
+                  cis.slice(nReco,nReco * 2), 
+                  cis.slice(nReco * 2, nReco * 3));
+              setCiRecoState(cir);
+            }
+          });
+      }
+    },
+    [dataVersion, nReco, selectedCis]
+  );
 
   useEffect(() => {
     if (dataVersion !== undefined) {
